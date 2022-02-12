cf7e23a2d873cda151b547216fcd3201ac2c7285
Denis Lopatin <stalk.sdv@gmail.com>
2022-02-12 16:33:06 +0100

    Final working changes

--- src/ApplicationCore/Services/OrderService.cs
+++ src/ApplicationCore/Services/OrderService.cs
@@ -1,11 +1,16 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Ardalis.GuardClauses;
+using Azure.Messaging.ServiceBus;
 using Microsoft.eShopWeb.ApplicationCore.Entities;
 using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.eShopWeb.ApplicationCore.Specifications;
+using Newtonsoft.Json;
 
 namespace Microsoft.eShopWeb.ApplicationCore.Services;
 
@@ -49,5 +54,31 @@ public class OrderService : IOrderService
         var order = new Order(basket.BuyerId, shippingAddress, items);
 
         await _orderRepository.AddAsync(order);
+        var orderMessage = JsonConvert.SerializeObject(new { id = Guid.NewGuid(), date = order.OrderDate.UtcDateTime.ToString(), details = JsonConvert.SerializeObject(order.OrderItems.Select(i => new { name = i.ItemOrdered.ProductName, qty = i.Units })) });
+        //Send the order to DeliveryOrderProcessor function
+        HttpClient httpclient = new HttpClient();
+        HttpRequestMessage request = new HttpRequestMessage
+        {
+            Content = new StringContent(orderMessage, Encoding.UTF8, "application/json")
+        };
+        httpclient.DefaultRequestHeaders.Add("x-functions-key", "yQPxUXa0RHjnPEYfVofmeNhaJpquZIVN5npiO2EZDn7VqwEivUX7sA==");
+        var response = await httpclient.PostAsync($"https://deliveryorderporcessorfunc.azurewebsites.net/api/DeliveryOrderProcessor", request.Content);
+        //Send to event bus
+        await using var sbclient = new ServiceBusClient("Endpoint=sb://eshopsb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=wFMFyBA+9VrF0td4bA3+cwe048/r9Dj6NDwxTjWLMtU=");
+        ServiceBusSender sender = sbclient.CreateSender("orderprocessor");
+        try
+        {
+            var message = new ServiceBusMessage(orderMessage);
+            await sender.SendMessageAsync(message);
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+        }
+        finally
+        {
+            await sender.DisposeAsync();
+            await sbclient.DisposeAsync();
+        }
     }
 }
